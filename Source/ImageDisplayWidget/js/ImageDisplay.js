e1840918da3ac68a78f3505e404ea803b19421fc
Deepak Rai <deepakr@futurismtechnologies.com>
2015-08-20 22:29:43 +0530

    Resizing images on page load

--- Source/ImageDisplayWidget/js/ImageDisplay.js
+++ Source/ImageDisplayWidget/js/ImageDisplay.js
@@ -210,6 +210,43 @@ $.fn.extend({
     },
     applycss: function (obj) {
         $(obj).parent().find('img').addClass('imagesize');
+    },
+    resizeImage: function () {
+        $('img').each(function () {
+            var img = this;
+
+            img.onload = function () {
+
+                var maxWidth = $(this).parent().find('img').attr('data-width-constraint'); // Max width for the image
+                var maxHeight = $(this).parent().find('img').attr('data-height-constraint');    // Max height for the image
+                var ratio = 0;  // Used for aspect ratio
+                var width = img.naturalWidth;    // Current image width
+                var height = img.naturalHeight;  // Current image height
+
+                // Check if the current width is larger than the max
+                if (width > maxWidth) {
+                    ratio = maxWidth / width;   // get ratio for scaling image
+                    $(this).parent().find('img').css("width", maxWidth); // Set new width
+                    $(this).parent().find('img').css("height", height * ratio);  // Scale height based on ratio
+                    height = height * ratio;    // Reset height to match scaled image
+                    width = width * ratio;    // Reset width to match scaled image
+                }
+
+                // Check if current height is larger than max
+                if (height > maxHeight) {
+                    ratio = maxHeight / height; // get ratio for scaling image
+                    $(this).parent().find('img').css("height", maxHeight);   // Set new height
+                    $(this).parent().find('img').css("width", width * ratio);    // Scale width based on ratio
+                    width = width * ratio;    // Reset width to match scaled image
+                    height = height * ratio;    // Reset height to match scaled image
+                }
+            }
+        });
     }
 })
 
+
+
+$(function () {
+    $().resizeImage();
+});
