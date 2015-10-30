7fc82d9fab416c95435c5d08e724929bbd8adc81
iq-mschmeets <mschmeets@Tigger.local>
2015-10-30 09:44:32 -0400

    Merge
    Deepak’s changes to gantt, with my add for supporting name lookups on
    filters.

--- Source/GranttChart/js/GranttPlugin.js
+++ Source/GranttChart/js/GranttPlugin.js
@@ -12,90 +12,94 @@
         };
         if (options) { $.extend(config, options); }
 
-        if (options.dataURL == null || options.el == '' || options.startDateMetaColumn == '' || options.endDateMetaColumn == '') {
+        if ((options.dataURL == null && options.name_lookup == null) 
+            || options.el == '' 
+            || options.startDateMetaColumn == '' 
+            || options.endDateMetaColumn == '') {
             console.log('GranttPlugin: Please pass all parameters.');
         }
 
-        $.ajax({
-            url: options.dataURL,
-            dataType: "text",
-            success: function (data) {
+        if( options.name_lookup ){
+            $.getJSON('../request/getid?name_lookup='+options.name_lookup)
+             .always( function( resp ){
+                load(  '../resource/filter/'+resp.textID+'/data' );
+             })
+        } else {
+            load( options.dataURL );
+        }
 
-                var jsonData = jQuery.parseJSON(data);
-                var finalData = [];
-                var item;
-                var counter = 1;
-                var meta;
-                var descIndex;
+        function load( dataURL ){
 
-                $(jsonData.data).each(function () {
-                    counter = 1;
-                    item = this;
-                    meta = jsonData.meta;
+            $.ajax({
+                url: dataURL,
+                dataType: "text",
+                success: function (data) {
 
+                    var jsonData = jQuery.parseJSON(data);
+                    var finalData = [];
+                    var item;
+                    var counter = 0;
+                    var meta;
 
-                    $(meta).each(function () {
-                        if (this.group == 0 && this.meta == "ELEMENT.DESCRIPTION") {
-                            descIndex = counter;
-                            return;
-                        }
+                    $(jsonData.data).each(function () {
+                        item = this;
+                        meta = jsonData.meta;
+
+                        //$(item.d).each(function () {
+
+                        //    if (meta[counter].display == true) {
+
+                        var granttItemValues = [];
+                        var granttItemDet = new Object();
+                        granttItemDet.from = item.d[5];
+                        granttItemDet.to = item.d[7];
+                        granttItemDet.label = item.d[1];
+                        granttItemDet.customClass = item.d[10];
+                        granttItemValues.push(granttItemDet);
+
+                        var granttItem = new Object();
+                        granttItem.name = item.d[1];
+                        granttItem.desc = item.d[3];
+                        granttItem.values = granttItemValues;
+
+                        finalData.push(granttItem);
+                        //    }
+                        //});
                         counter++;
                     });
 
-                    //$(item.d).each(function () {
+                    console.log(finalData);
+                    
+                    // Now that the data is transformed, invoke the
+                    // gantt plugin.
+                    $(options.el).gantt({
+                        source: finalData,
+                        scale: "weeks",
+                        minScale: "weeks",
+                        maxScale: "months",
+                        onItemClick: function (data) {
+                            alert("Item clicked - show some details");
+                        },
+                        onAddClick: function (dt, rowId) {
+                            alert("Empty space clicked - add an item!");
+                        },
+                        onRender: function () {
+                            console.log("chart rendered");
+                        }
+                    });
 
-                    //    if (meta[counter].display == true) {
+                    prettyPrint();
 
-                    var granttItemValues = [];
-                    var granttItemDet = new Object();
-                    granttItemDet.from = item.d[5];
-                    granttItemDet.to = item.d[7];
-                    granttItemDet.label = item.d[descIndex];
-                    granttItemDet.customClass = item.d[10];
-                    granttItemValues.push(granttItemDet);
+                    return data;
+                },
+                error: function (XHR, textStatus, errorThrown) {
+                    alert("error: " + errorThrown);
+                }
+            });
 
-                    var granttItem = new Object();
-                    granttItem.name = item.d[descIndex];
-                    granttItem.desc = item.d[3];
-                    granttItem.values = granttItemValues;
-
-                    finalData.push(granttItem);
-                    //    }
-                    //});
-
-                });
-
-                console.log(finalData);
-
-                $(options.el).gantt({
-                    source: finalData,
-                    scale: "weeks",
-                    minScale: "weeks",
-                    maxScale: "months",
-                    onItemClick: function (data) {
-                        alert("Item clicked - show some details");
-                    },
-                    onAddClick: function (dt, rowId) {
-                        alert("Empty space clicked - add an item!");
-                    },
-                    onRender: function () {
-                        console.log("chart rendered");
-                    }
-                });
-
-                prettyPrint();
-
-                return data;
-            },
-            error: function (XHR, textStatus, errorThrown) {
-                alert("error: " + errorThrown);
-            }
-        });
-
-
+        } // end loadData.
 
         return this;
-
     };
 
 }(jQuery));
