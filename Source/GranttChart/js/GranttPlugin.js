815072c9f68595ca23abf9084ca0e55abfc7a89e
Deepak Rai <deepakr@futurismtechnologies.com>
2015-11-03 21:07:02 +0530

    Added Timeline files
    These files were checked in earlier but due to some tech reason it was
    not showing in Git. Checkin again.

--- Source/GranttChart/js/GranttPlugin.js
+++ Source/GranttChart/js/GranttPlugin.js
@@ -12,94 +12,90 @@
         };
         if (options) { $.extend(config, options); }
 
-        if ((options.dataURL == null && options.name_lookup == null) 
-            || options.el == '' 
-            || options.startDateMetaColumn == '' 
-            || options.endDateMetaColumn == '') {
+        if (options.dataURL == null || options.el == '' || options.startDateMetaColumn == '' || options.endDateMetaColumn == '') {
             console.log('GranttPlugin: Please pass all parameters.');
         }
 
-        if( options.name_lookup ){
-            $.getJSON('../request/getid?name_lookup='+options.name_lookup)
-             .always( function( resp ){
-                load(  '../resource/filter/'+resp.textID+'/data' );
-             })
-        } else {
-            load( options.dataURL );
-        }
+        $.ajax({
+            url: options.dataURL,
+            dataType: "text",
+            success: function (data) {
 
-        function load( dataURL ){
+                var jsonData = jQuery.parseJSON(data);
+                var finalData = [];
+                var item;
+                var counter = 1;
+                var meta;
+                var descIndex;
 
-            $.ajax({
-                url: dataURL,
-                dataType: "text",
-                success: function (data) {
+                $(jsonData.data).each(function () {
+                    counter = 1;
+                    item = this;
+                    meta = jsonData.meta;
 
-                    var jsonData = jQuery.parseJSON(data);
-                    var finalData = [];
-                    var item;
-                    var counter = 0;
-                    var meta;
 
-                    $(jsonData.data).each(function () {
-                        item = this;
-                        meta = jsonData.meta;
-
-                        //$(item.d).each(function () {
-
-                        //    if (meta[counter].display == true) {
-
-                        var granttItemValues = [];
-                        var granttItemDet = new Object();
-                        granttItemDet.from = item.d[5];
-                        granttItemDet.to = item.d[7];
-                        granttItemDet.label = item.d[1];
-                        granttItemDet.customClass = item.d[10];
-                        granttItemValues.push(granttItemDet);
-
-                        var granttItem = new Object();
-                        granttItem.name = item.d[1];
-                        granttItem.desc = item.d[3];
-                        granttItem.values = granttItemValues;
-
-                        finalData.push(granttItem);
-                        //    }
-                        //});
+                    $(meta).each(function () {
+                        if (this.group == 0 && this.meta == "ELEMENT.DESCRIPTION") {
+                            descIndex = counter;
+                            return;
+                        }
                         counter++;
                     });
 
-                    console.log(finalData);
-                    
-                    // Now that the data is transformed, invoke the
-                    // gantt plugin.
-                    $(options.el).gantt({
-                        source: finalData,
-                        scale: "weeks",
-                        minScale: "weeks",
-                        maxScale: "months",
-                        onItemClick: function (data) {
-                            alert("Item clicked - show some details");
-                        },
-                        onAddClick: function (dt, rowId) {
-                            alert("Empty space clicked - add an item!");
-                        },
-                        onRender: function () {
-                            console.log("chart rendered");
-                        }
-                    });
+                    //$(item.d).each(function () {
 
-                    prettyPrint();
+                    //    if (meta[counter].display == true) {
 
-                    return data;
-                },
-                error: function (XHR, textStatus, errorThrown) {
-                    alert("error: " + errorThrown);
-                }
-            });
+                    var granttItemValues = [];
+                    var granttItemDet = new Object();
+                    granttItemDet.from = item.d[5];
+                    granttItemDet.to = item.d[7];
+                    granttItemDet.label = item.d[descIndex];
+                    granttItemDet.customClass = item.d[10];
+                    granttItemValues.push(granttItemDet);
 
-        } // end loadData.
+                    var granttItem = new Object();
+                    granttItem.name = item.d[descIndex];
+                    granttItem.desc = item.d[3];
+                    granttItem.values = granttItemValues;
+
+                    finalData.push(granttItem);
+                    //    }
+                    //});
+
+                });
+
+                console.log(finalData);
+
+                $(options.el).gantt({
+                    source: finalData,
+                    scale: "weeks",
+                    minScale: "weeks",
+                    maxScale: "months",
+                    onItemClick: function (data) {
+                        alert("Item clicked - show some details");
+                    },
+                    onAddClick: function (dt, rowId) {
+                        alert("Empty space clicked - add an item!");
+                    },
+                    onRender: function () {
+                        console.log("chart rendered");
+                    }
+                });
+
+                prettyPrint();
+
+                return data;
+            },
+            error: function (XHR, textStatus, errorThrown) {
+                alert("error: " + errorThrown);
+            }
+        });
+
+
 
         return this;
+
     };
 
 }(jQuery));
